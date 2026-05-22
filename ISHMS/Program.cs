3f151b497cd42bd1e20a76f5728d9a4103d4545b
Mohamed Talaat <mot141414@gmail.com>
2026-05-23 02:57:55 +0300

    changes for deployment on azure

--- ISHMS/Program.cs
+++ ISHMS/Program.cs
@@ -18,33 +18,33 @@ using ISHMS.BLL.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-            // Add services
-            builder.Services.AddControllers();
-            // Swagger With JWT Support
+// Add services
+builder.Services.AddControllers();
+// Swagger With JWT Support
 
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(options =>
-            {
-                options.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "ISHMS API",
-                    Version = "v1"
-                });
-
-                // ‰÷Ì› “—«— Authorize ›Ì Swagger
-                options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                {
-                    Name = "Authorization",
-                    Type = SecuritySchemeType.ApiKey,
-                    Scheme = "Bearer",
-                    BearerFormat = "JWT",
-                    In = ParameterLocation.Header,
-                    Description = "«ﬂ »: Bearer {token}"
-                    // „À«·: Bearer eyJhbGciOiJIUzI1NiIs...
-                });
-
-                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
     {
+        Title = "ISHMS API",
+        Version = "v1"
+    });
+
+    // ‰÷Ì› “—«— Authorize ›Ì Swagger
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "«ﬂ »: Bearer {token}"
+        // „À«·: Bearer eyJhbGciOiJIUzI1NiIs...
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+{
         {
             new OpenApiSecurityScheme
             {
@@ -56,125 +56,140 @@ var builder = WebApplication.CreateBuilder(args);
             },
             Array.Empty<string>()
         }
+});
+});
+
+
+// DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("Default")
+    ));
+
+// ====================================================
+//  ASP.NET Identity
+// ====================================================
+
+builder.Services
+    .AddIdentity<ApplicationUser, IdentityRole>(options =>
+    {
+        options.Password.RequireDigit = true;
+
+        options.Password.RequireLowercase = true;
+
+        options.Password.RequireUppercase = true;
+
+        options.Password.RequireNonAlphanumeric = false;
+
+        options.Password.RequiredLength = 6;
+
+        options.User.RequireUniqueEmail = true;
+    })
+    .AddEntityFrameworkStores<AppDbContext>();
+
+
+// ====================================================
+// 3. JWT Settings
+// ====================================================
+
+builder.Services.Configure<JwtSettings>(
+    builder.Configuration.GetSection("JwtSettings"));
+
+
+// ====================================================
+// 4. JWT Authentication
+// ====================================================
+
+var jwtSettings = builder.Configuration
+                         .GetSection("JwtSettings")
+                         .Get<JwtSettings>()!;
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+
+            ValidateAudience = true,
+
+            ValidateLifetime = true,
+
+            ValidateIssuerSigningKey = true,
+
+            ValidIssuer = jwtSettings.Issuer,
+
+            ValidAudience = jwtSettings.Audience,
+
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtSettings.Key))
+        };
     });
-            });
-
-
-            // DbContext
-            builder.Services.AddDbContext<AppDbContext>(options =>
-                options.UseSqlServer(
-                    builder.Configuration.GetConnectionString("Default")
-                ));
-
-            // ====================================================
-            //  ASP.NET Identity
-            // ====================================================
-
-            builder.Services
-                .AddIdentity<ApplicationUser, IdentityRole>(options =>
-                {
-                    options.Password.RequireDigit = true;
-
-                    options.Password.RequireLowercase = true;
-
-                    options.Password.RequireUppercase = true;
-
-                    options.Password.RequireNonAlphanumeric = false;
-
-                    options.Password.RequiredLength = 6;
-
-                    options.User.RequireUniqueEmail = true;
-                })
-                .AddEntityFrameworkStores<AppDbContext>();
-
-
-            // ====================================================
-            // 3. JWT Settings
-            // ====================================================
-
-            builder.Services.Configure<JwtSettings>(
-                builder.Configuration.GetSection("JwtSettings"));
-
-
-            // ====================================================
-            // 4. JWT Authentication
-            // ====================================================
-
-            var jwtSettings = builder.Configuration
-                                     .GetSection("JwtSettings")
-                                     .Get<JwtSettings>()!;
-
-            builder.Services
-                .AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-
-                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(options =>
-                {
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuer = true,
-
-                        ValidateAudience = true,
-
-                        ValidateLifetime = true,
-
-                        ValidateIssuerSigningKey = true,
-
-                        ValidIssuer = jwtSettings.Issuer,
-
-                        ValidAudience = jwtSettings.Audience,
-
-                        IssuerSigningKey = new SymmetricSecurityKey(
-                            Encoding.UTF8.GetBytes(jwtSettings.Key))
-                    };
-                });
 
 
 
-            // Dependency Injection
-            builder.Services.AddScoped<IPatientRepository, PatientRepository>();
-            builder.Services.AddScoped<IPatientService, PatientService>();
-            builder.Services.AddScoped<IAuthRepository, AuthRepository>();
-            builder.Services.AddScoped<IAuthService, AuthService>();
-            builder.Services.AddScoped<NewsService>();
+// Dependency Injection
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped<IAuthRepository, AuthRepository>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<NewsService>();
 //department, bed
-            builder.Services.AddScoped<IBedService, BedService>();
-            builder.Services.AddScoped<IDepartmentService, DepartmentService>();
-             builder.Services.AddScoped<NewsService>();
+builder.Services.AddScoped<IBedService, BedService>();
+builder.Services.AddScoped<IDepartmentService, DepartmentService>();
+builder.Services.AddScoped<NewsService>();
 //ward, room, bed
-           //builder.Services.AddScoped<WardService>();
-            //builder.Services.AddScoped<RoomService>();
-            builder.Services.AddScoped<BedService>();
-            builder.Services.AddScoped<IPatientTaskService, PatientTaskService>();
-            builder.Services.AddScoped<IAlertService, AlertService>();
-            builder.Services.AddScoped<IWorkflowService, WorkflowService>();
-            builder.Services.AddScoped<IMedicalReportService, MedicalReportService>();
-
-            var app = builder.Build();
+//builder.Services.AddScoped<WardService>();
+//builder.Services.AddScoped<RoomService>();
+builder.Services.AddScoped<BedService>();
+builder.Services.AddScoped<IPatientTaskService, PatientTaskService>();
+builder.Services.AddScoped<IAlertService, AlertService>();
+builder.Services.AddScoped<IWorkflowService, WorkflowService>();
+builder.Services.AddScoped<IMedicalReportService, MedicalReportService>();
 
 
-  
 
-            // Seed Roles first time
+builder.Services.AddCors(options =>
+{
+options.AddPolicy("AllowAll",
+policy =>
+{
+    policy.AllowAnyOrigin()
+          .AllowAnyHeader()
+          .AllowAnyMethod();
+});
+});
 
-            using (var scope = app.Services.CreateScope())
-            {
-                var roleManager = scope.ServiceProvider
-                                       .GetRequiredService<RoleManager<IdentityRole>>();
+var app = builder.Build();
 
-                string[] roles = { "Admin", "Doctor", "Nurse", "Receptionist" };
 
-                foreach (var role in roles)
-                {
-                    if (!await roleManager.RoleExistsAsync(role))
-                    {
-                        await roleManager.CreateAsync(new IdentityRole(role));
-                    }
-                }
-            }
+
+
+
+
+// Seed Roles first time
+
+using (var scope = app.Services.CreateScope())
+{
+    var roleManager = scope.ServiceProvider
+                           .GetRequiredService<RoleManager<IdentityRole>>();
+
+    string[] roles = { "Admin", "Doctor", "Nurse", "Receptionist" };
+
+    foreach (var role in roles)
+    {
+        if (!await roleManager.RoleExistsAsync(role))
+        {
+            await roleManager.CreateAsync(new IdentityRole(role));
+        }
+    }
+}
 
 // Seed departments and rooms and beds
 
@@ -224,19 +239,22 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Middleware
-if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger();
+app.UseSwaggerUI();
 
-            app.UseHttpsRedirection();
+//}          
 
-            app.UseAuthentication();
 
-            app.UseAuthorization();
+app.UseCors("AllowAll");
+app.UseHttpsRedirection();
 
-            app.MapControllers();
+app.UseAuthentication();
 
-            app.Run();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
 
